2127854dc4c1a75b39b6f66c6f09f41c26e0cd9c
prayogo santoso <prayogo.santoso681@gmail.com>
2025-09-12 23:17:21 +0700

    remove unuse code

--- internal/handlers/file.go
+++ internal/handlers/file.go
@@ -67,38 +67,3 @@ func (uc *fileHandler) Post(ctx *fiber.Ctx) error {
 
 	return ctx.Status(fiber.StatusOK).JSON(files)
 }
-
-// import (
-// 	"context"
-// 	"mime/multipart"
-// 	"tutuplapak-user/internal/config"
-// 	"tutuplapak-user/internal/dto"
-
-// 	minioUploader "github.com/arieffadhlan/go-fitbyte/internal/pkg/minio"
-// 	"github.com/minio/minio-go/v7"
-// )
-
-// type UseCase interface {
-// 	UploadFile(context.Context, *multipart.FileHeader, multipart.File, string)(dto.File, error)
-// }
-
-// type useCase struct{
-// 	config config.Config
-// 	minio *minio.Client
-// }
-
-// func NewUseCase(config config.Config) UseCase {
-// 	minioConfig := &minioUploader.MinioConfig{
-// 		AccessKeyID:     config.,
-// 		SecretAccessKey: config.Minio.SecretAccessKey,
-// 		UseSSL:          config.Minio.UseSSL,
-// 		Endpoint:        config.Minio.Endpoint,
-// 	}
-
-// 	minioClient, _ := minioUploader.NewUploader(minioConfig)
-
-// 	return &useCase{
-// 		config: config,
-// 		minio:  minioClient,
-// 	}
-// }
