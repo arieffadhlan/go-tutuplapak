990016b5f6dc32f39d2f0a7c4d38ed4c8ea26b7a
prayogo santoso <prayogo.santoso681@gmail.com>
2025-09-12 23:10:09 +0700

    create file response type

--- internal/dto/file.go
+++ internal/dto/file.go
@@ -3,8 +3,14 @@ package dto
 import "time"
 
 type File struct {
-	ID           string    `db:"id"`
+	ID           string    `db:"id" `
 	Url          string    `db:"url"`
 	ThumbnailUrl string    `db:"thumbnail_url"`
 	CreateAt     time.Time `db:"created_at"`
+}
+
+type FileResponse struct {
+	ID           string `json:"fileId"`
+	Url          string `json:"fileUri"`
+	ThumbnailUrl string `json:"fileThumbnailUri"`
 }
